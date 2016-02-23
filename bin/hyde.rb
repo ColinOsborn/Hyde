289183c662ae5d9a54aaa83a582bf859b60cdb33
Christopher Soden <karavol27@gmail.com>
2016-02-22 23:17:45 -0700

    Preliminary Implementation of file structure

--- bin/hyde.rb
+++ bin/hyde.rb
@@ -5,11 +5,20 @@ class Hyde
   attr_accessor :dir
 
   def initialize(filename)
-    filename.chomp!
     if Dir.exist?(filename)
       puts "Error. Directory #{filename} already exists."
     else
       Dir.mkdir(filename)
+      Dir.mkdir(filename + "/_output")
+      Dir.mkdir(filename + "/source")
+      Dir.mkdir(filename + "/source/css")
+      Dir.mkdir(filename + "/source/css/main.css")
+      Dir.mkdir(filename + "/source/index.markdown")
+      Dir.mkdir(filename + "/source/pages")
+      Dir.mkdir(filename + "/source/pages/about.markdown")
+      Dir.mkdir(filename + "/source/posts")
+      t = Time.new
+      Dir.mkdir(filename + "/source/posts/" + t.strftime("%F") + "-welcome-to-hyde.markdown")
     end
   end
 
@@ -19,7 +28,12 @@ end
 
 
 if __FILE__ ==$0
-  filename = gets
+  if ARGV[0] == nil
+    filename = gets
+    filename.chomp!
+  else
+    filename = ARGV[0]
+  end
   h=Hyde.new(filename)
 
 end
