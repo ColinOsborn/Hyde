137887c2bc27f1d0891068b709cfbc527dc56081
Christopher Soden <karavol27@gmail.com>
2016-02-22 22:38:31 -0700

    It is creating a given filename in the current directory

--- bin/hyde.rb
+++ bin/hyde.rb
@@ -1,7 +1,34 @@
 #!/usr/bin/env ruby
 
-args = ARGV
-puts "You ran Hyde with these arguments: #{args}"
+class Hyde
+
+  attr_accessor :dir
+
+  def initialize(filename)
+    filename.chomp!
+    if Dir.exist?(filename)
+      puts "Error. Directory #{filename} already exists."
+    else
+      Dir.mkdir(filename)
+    end
+  end
+
+end
+
+
+
+
+if __FILE__ ==$0
+  filename = gets
+  h=Hyde.new(filename)
+
+end
+
+
+
+
+# args = ARGV
+# puts "You ran Hyde with these arguments: #{args}"
 
 # ##############
 # Your code here
