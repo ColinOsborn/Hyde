c68e7281f4fe543c7ade72ff7773cd74a532c085
Colin Osborn <colin.osborn@gmail.com>
2016-02-22 22:27:35 -0700

    start

---
+++ lib/hyde.rb
@@ -0,0 +1,8 @@
+class Hyde
+ def initialize(method, location)
+  Dir.mkdir("method")
+  p "#{method}#{location}"
+ end
+
+ 
+end
---
+++ test/hyde_test.rb
@@ -0,0 +1,6 @@
+
+gem 'minitest'
+require_relative '../bin/hyde'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
