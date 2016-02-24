ce36d87d0f661f993a7bf658d62804459b6541d0
Christopher Soden <karavol27@gmail.com>
2016-02-24 08:33:30 -0700

    starting test dev after downloading files

--- bin/hyde.rb
+++ bin/hyde.rb
@@ -1,29 +1,85 @@
-#!/usr/bin/env ruby
+class FileManager
 
-class Hyde
 
-  attr_accessor :dir
-
-  def initialize(filename)
-    filename.chomp!
+  def create_new_site_structure(filename)
     if Dir.exist?(filename)
       puts "Error. Directory #{filename} already exists."
     else
       Dir.mkdir(filename)
+      Dir.mkdir(filename + "/_output")
+      Dir.mkdir(filename + "/source")
+      make_basic_folders(filename + "/source")
+      make_starting_files(filename + "/source")
     end
   end
 
+  def make_basic_folders(filename)
+    Dir.mkdir(filename + "/css")
+    Dir.mkdir(filename + "/pages")
+    Dir.mkdir(filename + "/posts")
+  end
+
+  def make_starting_files(filename)
+    t = Time.new
+    File.write(filename + "/css/main.css", "")
+    File.write(filename + "/index.markdown", "")
+    File.write(filename + "/pages/about.markdown", "")
+    File.write(filename + "/posts/" + t.strftime("%F") + "-welcome-to-hyde.markdown", "")
+  end
+
+
+
+
 end
 
+class Build
+
+  def build_method(file_manager, filename)
+    file_manager.make_basic_folders(filename)
+
+  end
+
+    # Create file structure in the output folder using FileManager
+
+    # Call FileManager to populate files from source to outpu
+
+    # Use FileManager to iterate through the files,
+    #  # Processing Markdown and put resulting HTML in output
+
+
+  end
 
 
 
 if __FILE__ ==$0
-  filename = gets
-  h=Hyde.new(filename)
-
+  case ARGV[0]
+  when nil
+    puts "No command given"
+  when "new"
+    if ARGV[1] == nil
+      puts "No filename given"
+      # filename = gets
+      # filename.chomp!
+    else
+      filename = ARGV[1]
+    end
+    f=FileManager.new
+    f.create_new_site_structure(filename)
+  end
 end
 
+
+
+
+
+  # if ARGV[0] == nil
+  #   puts "No command given"
+  # elsif ARGV[0] == "new"
+  #
+  #
+  #
+  # end
+  # end
 
 
 
@@ -33,3 +89,13 @@ end
 # ##############
 # Your code here
 # ##############
+
+#
+# class Hyde
+#  def initialize(method, location)
+#   Dir.mkdir("method")
+#   p "#{method}#{location}"
+#  end
+#
+#
+# end
---
+++ test/file_manager_test.rb
@@ -0,0 +1,58 @@
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require_relative '../bin/hyde'
+
+class HydeTest < MiniTest::Test
+
+  def test_it_accepts_the_new_command
+    assert hyde = Hyde.new("testdir")
+  end
+
+  def test_it_returns_an_error_if_it_is_given_new_for_a_directory_that_already_exists
+
+  end
+
+  def test_it_creates_the_project_at_the_given_file_path
+
+  end
+
+  def test_it_creates_underscore_output_as_a_top_level_folder
+
+  end
+
+  def test_it_creates_source_as_a_top_level_folder
+
+  end
+
+  def test_inside_source_it_creates_a_css_folder
+
+  end
+
+  def test_inside_source_it_creates_an_index_dot_markdown_folder
+
+  end
+
+  def test_inside_source_it_creates_a_pages_folder
+
+  end
+
+  def test_inside_source_it_creates_a_posts_folder
+
+  end
+
+  def test_inside_css_it_creates_a_main_dot_css_folder
+
+  end
+
+  def test_inside_pages_it_creates_an_about_dot_markdown_folder
+
+  end
+
+  def test_inside_posts_it_creates_a_year_month_date_welcome_to_hyde_dot_markdown_folder
+
+  end
+
+
+end
