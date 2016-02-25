1f9010209aca2ef7bff614f8a4e048a04ae71e7c
Colin Osborn <Colin.Osborn@gmail.com>
2016-02-25 01:03:49 -0700

    iteration 2

--- lib/hyde.rb
+++ lib/hyde.rb
@@ -1,4 +1,7 @@
-
+require 'fileutils'
+require 'kramdown'
+require_relative 'file_reader'
+require_relative 'file_writer'
 
 
 class FileManager
@@ -21,6 +24,7 @@ class FileManager
     Dir.mkdir(filename + "/css")
     Dir.mkdir(filename + "/pages")
     Dir.mkdir(filename + "/posts")
+    Dir.mkdir(filename + "/layouts")
   end
 
   def make_starting_files(filename)
@@ -38,39 +42,67 @@ class SiteBuilder
 
   def initialize
     @file_manager = FileManager.new
+    @reader = FileReader.new
+    @writer = FileWriter.new
   end
 
   def build(filename)
-    file_manager.make_basic_folders(filename)
-
+    FileUtils.copy_entry(filename + "/source/", filename + "/_output/")
+    markdown_files = []
+    markdown_files += Dir.glob(filename + "/_output/*{.markdown,.md}")
+    markdown_files += Dir.glob(filename + "/_output/*/*{.markdown,.md}")
+    markdown_files.each do |filename|
+      convert_to_html(filename)
+    end
   end
 
-    # Create file structure in the output folder using FileManager
+  def convert_to_html(filename)
+    content = @reader.read(filename)
+    html_text = Kramdown::Document.new(content).to_html
+    @writer.write(filename, html_text)
+  end
 
-    # Call FileManager to populate files from source to outpu
 
-    # Use FileManager to iterate through the files,
-    #  # Processing Markdown and put resulting HTML in output
+  # def build_method(file_manager, filename)
+  #   file_manager.make_basic_folders(filename)
+  # end
 
+  def make_basic_files_output
+    Dir.mkdir(filename + "/css")
+    Dir.mkdir(filename + "/pages")
+    Dir.mkdir(filename + "/posts")
+    Dir.mkdir(filename + "/layouts")
+  end
+
+  def make_output_files
+    t = Time.new
+    File.write(filename + "/css/main.css", "")
+    File.write(filename + "/index.markdown", "")
+    File.write(filename + "/pages/about.markdown", "")
+    File.write(filename + "/posts/" + t.strftime("%F") + "-welcome-to-hyde.markdown", "")
+  end
 
   end
 
 
 
 if __FILE__ ==$0
-  case ARGV[0]
-  when nil
-    puts "No command given"
-  when "new"
-    if ARGV[1] == nil
-      puts "No filename given"
-      # filename = gets
-      # filename.chomp!
-    else
-      filename = ARGV[1]
+  if ARGV[1] == nil
+    puts "No filename given"
+  else
+    filename = ARGV[1]
+    case ARGV[0]
+    when nil
+      puts "No command given"
+    when "new"
+        # filename = gets
+        # filename.chomp!
+      f=FileManager.new
+      f.create_new_site_structure(filename)
+    when "build"
+      b = SiteBuilder.new
+      b.build(filename)
     end
-    f=FileManager.new
-    f.create_new_site_structure(filename)
   end
 end
 
