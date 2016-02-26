03b9c1bf4c3e8f2c66b6a786935f5bb6f6f11551
Christopher Soden <karavol27@gmail.com>
2016-02-25 17:08:37 -0700

    changed blogpop

--- lib/blog_pop.rb
+++ lib/blog_pop.rb
@@ -15,16 +15,16 @@ class BlogPop
   end
 
   def create_post(file_path, post_title)
-    # 1 - read the layout file from source/layouts/default.html.erb into our template string
-    body = builder.convert_to_html(file_path)
-    render_html_with_template(file_path, body, post_title)
+    # body = builder.convert_to_html(file_path)
+    render_html_with_template(file_path, post_title)
     # html_file_path = #markdown file located in correct directory
   end
 
-  def render_html_with_template(file_path, body, post_title)
+  def render_html_with_template(file_path, post_title)
+    # 1 - read the layout file from source/layouts/default.html.erb into our template string
     erb_template = File.read("#{file_path}/source/layouts/default.html.erb")
-    post_title_no_dash = post_title.gsub("-", " ")
-    formatted_html = ERB.new(erb_template).result(binding)
-    File.write("#{file_path}/_output/posts/#{@date}-#{post_title}.html", formatted_html)
+    post_title_no_dash = builder.convert_to_html( post_title.gsub("-"," ") )
+    body = ERB.new(erb_template).result(binding)
+    File.write("#{file_path}/_output/posts/#{@date}-#{post_title}.html", body)
   end
 end
